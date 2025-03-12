6de3bc2c2b11b5fdd30f3f079e6e5eb0f45bc58b
Diego de Mello <diegodemello027@gmail.com>
2025-03-11 21:03:31 -0300

    Criando os components.

---
+++ src/app/components/ad-bar/ad-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-bar',
+  templateUrl: './ad-bar.component.html',
+  styleUrls: ['./ad-bar.component.css']
+})
+export class AdBarComponent {
+
+}

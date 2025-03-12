4273e99d381f47ec8cd93c9005a09d88486c7469
Diego de Mello <diegodemello027@gmail.com>
2025-03-12 19:38:21 -0300

    Adicionando responsividade.

--- src/app/components/ad-bar/ad-bar.component.ts
+++ src/app/components/ad-bar/ad-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ad-bar',
   templateUrl: './ad-bar.component.html',
-  styleUrls: ['./ad-bar.component.css']
+  styleUrls: ['./ad-bar.component.css', './ad-bar.responsive.component.css']
 })
 export class AdBarComponent {
 
